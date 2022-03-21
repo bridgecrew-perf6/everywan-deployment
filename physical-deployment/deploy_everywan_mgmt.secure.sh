b0cd4a09a6da0c3af1738dcaf9a0287f59fc69eb
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2022-03-21 17:59:51 +0100

    Persist keystone-database data on volume

--- physical-deployment/deploy_everywan_mgmt.secure.sh
+++ physical-deployment/deploy_everywan_mgmt.secure.sh
@@ -3,6 +3,9 @@
 # Create a folder to store MongoDB data
 sudo mkdir -p /var/everywan/mongodb
 
+# Create a folder to store Keystone data
+sudo mkdir -p /var/everywan/keystone-database
+
 cd ../everywan-dockerized
 docker-compose -f docker-compose.physical.secure.yml build keystone everygui certbot cron
 docker-compose -f docker-compose.physical.secure.yml pull database mongodb everyboss everyedgeos
--- physical-deployment/deploy_everywan_mgmt.sh
+++ physical-deployment/deploy_everywan_mgmt.sh
@@ -3,6 +3,9 @@
 # Create a folder to store MongoDB data
 sudo mkdir -p /var/everywan/mongodb
 
+# Create a folder to store Keystone data
+sudo mkdir -p /var/everywan/keystone-database
+
 cd ../everywan-dockerized
 docker-compose -f docker-compose.physical.yml build keystone
 docker-compose -f docker-compose.physical.yml pull database mongodb everyboss everyedgeos everygui
