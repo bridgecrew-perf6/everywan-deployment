86ac167825ac225914a3fa5f009279f4d0d9e44a
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2022-03-21 17:37:05 +0100

    Persist MongoDB data on a volume

--- physical-deployment/deploy_everywan_mgmt.secure.sh
+++ physical-deployment/deploy_everywan_mgmt.secure.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Create a folder to store MongoDB data
+sudo mkdir -p /var/everywan/mongodb
+
 cd ../everywan-dockerized
 docker-compose -f docker-compose.physical.secure.yml build keystone everygui certbot cron
 docker-compose -f docker-compose.physical.secure.yml pull database mongodb everyboss everyedgeos
--- physical-deployment/deploy_everywan_mgmt.sh
+++ physical-deployment/deploy_everywan_mgmt.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Create a folder to store MongoDB data
+sudo mkdir -p /var/everywan/mongodb
+
 cd ../everywan-dockerized
 docker-compose -f docker-compose.physical.yml build keystone
 docker-compose -f docker-compose.physical.yml pull database mongodb everyboss everyedgeos everygui
