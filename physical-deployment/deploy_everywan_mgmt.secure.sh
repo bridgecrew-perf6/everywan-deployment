6ff05f03d2dfc259867a69bd663da396d5d5a1fb
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2022-03-21 18:40:39 +0100

    Disable cache for EveryGUI to use the latest EveryGUI image

--- physical-deployment/deploy_everywan_mgmt.secure.sh
+++ physical-deployment/deploy_everywan_mgmt.secure.sh
@@ -7,6 +7,7 @@ sudo mkdir -p /var/everywan/mongodb
 sudo mkdir -p /var/everywan/keystone-database
 
 cd ../everywan-dockerized
-docker-compose -f docker-compose.physical.secure.yml build keystone everygui certbot cron
+docker-compose -f docker-compose.physical.secure.yml build --no-cache everygui
+docker-compose -f docker-compose.physical.secure.yml build keystone certbot cron
 docker-compose -f docker-compose.physical.secure.yml pull database mongodb everyboss everyedgeos
 docker-compose -f docker-compose.physical.secure.yml up --detach
