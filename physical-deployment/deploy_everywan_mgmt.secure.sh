34b7eba1ee3e86ee9267b684019293b7ccc2a5fe
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2022-03-21 18:51:59 +0100

    Change bind volumes to named volumes

--- physical-deployment/deploy_everywan_mgmt.secure.sh
+++ physical-deployment/deploy_everywan_mgmt.secure.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Create a folder to store MongoDB data
-sudo mkdir -p /var/everywan/mongodb
-
-# Create a folder to store Keystone data
-sudo mkdir -p /var/everywan/keystone-database
-
 cd ../everywan-dockerized
 docker-compose -f docker-compose.physical.secure.yml build --no-cache everygui
 docker-compose -f docker-compose.physical.secure.yml build keystone certbot cron
--- physical-deployment/deploy_everywan_mgmt.sh
+++ physical-deployment/deploy_everywan_mgmt.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Create a folder to store MongoDB data
-sudo mkdir -p /var/everywan/mongodb
-
-# Create a folder to store Keystone data
-sudo mkdir -p /var/everywan/keystone-database
-
 cd ../everywan-dockerized
 docker-compose -f docker-compose.physical.yml build keystone
 docker-compose -f docker-compose.physical.yml pull database mongodb everyboss everyedgeos everygui
